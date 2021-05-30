3a566506cf5eee1859976f55c91e8812ff00e6ef
kerm1t <wolfgang.schulz@gmail.com>
2021-05-30 13:05:24 +0200

    clean up

--- samples/glenz/glenz.cpp
+++ samples/glenz/glenz.cpp
@@ -4,9 +4,17 @@
 #define FBUF2D_HEIGHT 64 // HACK!! remove!!
 #include "zweidee_draw.h"
 #include "glenz.h"
-float z = 1.0;
-glm::vec3 tri[3] = { {5.,5.,z},{5.,21.,z},{25.,4.,z} };
-glm::vec3 tri2[3] = { {17.,22.,z},{4.,19.,z},{23.,5.,z} };
+
+/*float z = .9;
+glm::vec3 tri[3] = { {5.,3.,z},{5.,21.,z},{25.,1.,z} }; // white
+float z2 = -.9; 
+glm::vec3 tri2[3] = { {17.,22.,z2},{4.,19.,z2},{23.,3.,z2} };
+*/
+glm::vec3 tri[3] = { {0.,0.,0.2},{5.,0.,0.2},{5.,5.,0.2} }; // pink
+glm::vec3 tri2[3] = { {5.,5.,0.2},{5.,5.,5.},{0.,5.,0.2} };
+//glm::vec3 tri2[3] = { {0.,0.,0.2},{-5.,0.,0.2},{-5.,0.,5.} };
+
+
 
 int glenz::CGlenz::init()
 {
@@ -20,7 +28,7 @@ glm::vec2 rot2(glm::vec2 v, float theta)
   out.y = v.x * sin(theta) + v.y * cos(theta);
   return out;
 }
-glm::vec3 rot3x(glm::vec3 v, float theta)
+glm::vec3 rot3z(glm::vec3 v, float theta)
 {
   glm::vec3 out;
   out.x = v.x * cos(theta) - v.y * sin(theta) + 0;
@@ -28,15 +36,20 @@ glm::vec3 rot3x(glm::vec3 v, float theta)
   out.z = 0 + 0 + v.z;
   return out;
 }
-glm::vec3 rot3y(glm::vec3 v, float theta)
+glm::vec3 rot3x(glm::vec3 v, float theta)
 {
   glm::vec3 out;
+//  theta = 3.145f/2.0f;
   out.x = v.x + 0 + 0;
   out.y = 0 + v.y * cos(theta) - v.z * sin(theta);
   out.z = 0 + v.y * sin(theta) + v.z * cos(theta);
-  return out;
+/*  glm::mat3 r = { {1,0,0},
+                  {0,cos(theta),-sin(theta)},
+                  {0,sin(theta), cos(theta)} };
+  out =  r*v;
+  */return out;
 }
-glm::vec3 rot3z(glm::vec3 v, float theta)
+glm::vec3 rot3y(glm::vec3 v, float theta)
 {
   glm::vec3 out;
   out.x = v.x * cos(theta) + 0 + v.z * sin(theta);
@@ -44,68 +57,71 @@ glm::vec3 rot3z(glm::vec3 v, float theta)
   out.z = -v.x * sin(theta) + 0 + v.z * cos(theta);
   return out;
 }
-void glenz::CGlenz::lesson2(unsigned char * data) // draw 3 filled triangles
+
+// 2do: 
+zweidee::vec2 persp(glm::vec3 v, glm::vec2 center, float focal)
 {
-  zweidee::vec2 t0[3] = { zweidee::vec2(10, 20), zweidee::vec2(50, 40), zweidee::vec2(20, 30) };
-  zweidee::vec2 t1[3] = { zweidee::vec2(41, 12), zweidee::vec2(40, 1),  zweidee::vec2(20, 60) };
-  zweidee::vec2 t2[3] = { zweidee::vec2(30, 55), zweidee::vec2(30, 50), zweidee::vec2(40, 60) };
-  glm::vec3 red   = glm::vec3(255, 0, 0);
-  glm::vec3 white = glm::vec3(255, 255, 255);
-  glm::vec3 green = glm::vec3(0, 255, 0);
-  Triangle_filled(fbuf2d, t0[0], t0[1], t0[2], red, data);
-  Triangle_filled(fbuf2d, t1[0], t1[1], t1[2], white, data);
-  Triangle_filled(fbuf2d, t2[0], t2[1], t2[2], green, data);
+  zweidee::vec2 out;
+//#define ALMOST_ZERO 0.000001f // this is very slow
+#define ALMOST_ZERO 0.01f // this is not so slow
+  if (abs(v.z) < ALMOST_ZERO) v.z = ALMOST_ZERO;
+  out.x = center.x + v.x / v.z * focal;
+  out.y = center.y + v.y / v.z * focal;
+  return out;
 }
 
 // ---------------
 // main game cycle
 // ---------------
+// 2do:
+// - better rotation, maybe doing translation first
+// + draw axes r,g,b
+// 
 int glenz::CGlenz::doit(unsigned char * data)
 {
-//  lesson2(data);
-  fbuf2d->setpixel(data, 10, 10, 50, 100, 100); // bgr
+//  fbuf2d->setpixel(data, 10, 10, 50, 100, 100); // bgr
 
-  glm::vec3 col = {222,22,222};
-  int xc = 32; int yc = 32;
-  float f = 1.f;
-/*  int x1 = tri[0].x / tri[0].z * f;
-  int y1 = tri[0].y / tri[0].z * f;
-  int x2 = tri[1].x / tri[1].z * f;
-  int y2 = tri[1].y / tri[1].z * f;
-  Bresenham(fbuf2d, xc+x1, yc + y1, xc + x2, yc + y2, col, data);
-
-  int x3 = tri[2].x / tri[2].z * f;
-  int y3 = tri[2].y / tri[2].z * f;
-  Bresenham(fbuf2d, xc + x2, yc + y2, xc + x3, yc + y3, col, data);
-  Bresenham(fbuf2d, xc + x3, yc + y3, xc + x1, yc + y1, col, data);
-*/
-//  tri[0].z -= 0.01;
-//  tri[1].z -= 0.01;
-//  tri[2].z -= 0.01;
-  zweidee::vec2 t0[3];
-// there's a problem with rot3y and rot3z --> not anymore :-)
-  tri[0] = rot3x(tri[0], .06);
-  tri[1] = rot3x(tri[1], .06);
-  tri[2] = rot3x(tri[2], .06);
-  t0[0].x = xc + tri[0].x / tri[0].z * f;
-  t0[0].y = yc + tri[0].y / tri[0].z * f;
-  t0[1].x = xc + tri[1].x / tri[1].z * f;
-  t0[1].y = yc + tri[1].y / tri[1].z * f;
-  t0[2].x = xc + tri[2].x / tri[2].z * f;
-  t0[2].y = yc + tri[2].y / tri[2].z * f;
-  Triangle_filled(fbuf2d, t0[0], t0[1], t0[2], col, data);
-  
-  tri2[0] = rot3x(tri2[0], .03);
-  tri2[1] = rot3x(tri2[1], .03);
-  tri2[2] = rot3x(tri2[2], .03);
+  glm::vec3 pink = { 212,22,212 };
   glm::vec3 white = { 255,255,255 };
-  t0[0].x = xc + tri2[0].x / tri2[0].z * f;
-  t0[0].y = yc + tri2[0].y / tri2[0].z * f;
-  t0[1].x = xc + tri2[1].x / tri2[1].z * f;
-  t0[1].y = yc + tri2[1].y / tri2[1].z * f;
-  t0[2].x = xc + tri2[2].x / tri2[2].z * f;
-  t0[2].y = yc + tri2[2].y / tri2[2].z * f;
+
+// perspective projection
+  glm::vec2 center = { 32, 32 };
+  float f = 1.f;
+  zweidee::vec2 t0[3];
+
+// there's a problem with rot3y and rot3z --> not anymore :-)
+  zweidee::glenz = true;
+  float rot = 0.06;
+  tri[0] = rot3z(tri[0], rot);
+  tri[1] = rot3z(tri[1], rot);
+  tri[2] = rot3z(tri[2], rot);
+  t0[0] = persp(tri[0], center, f);
+  t0[1] = persp(tri[1], center, f);
+  t0[2] = persp(tri[2], center, f);
+  Triangle_filled(fbuf2d, t0[0], t0[1], t0[2], pink, data);
+  
+  tri2[0] = rot3z(tri2[0], .03);
+  tri2[1] = rot3z(tri2[1], .03);
+  tri2[2] = rot3z(tri2[2], .03);
+  t0[0] = persp(tri2[0], center, f);
+  t0[1] = persp(tri2[1], center, f);
+  t0[2] = persp(tri2[2], center, f);
   Triangle_filled(fbuf2d, t0[0], t0[1], t0[2], white, data);
+
+  // draw axes
+  zweidee::glenz = false;
+  glm::vec3 x[2] = { {0.,0.,0.},{.1,0.,0.} };
+  glm::vec3 y[2] = { {0.,0.,0.},{0.,.1f,0.} };
+  glm::vec3 z[2] = { {0.,0.,0.},{0.,0.,.1f} };
+  t0[0] = persp(x[0], center, f);
+  t0[1] = persp(x[1], center, f);
+  Bresenham(fbuf2d, t0[0].x, t0[0].y, t0[1].x, t0[1].y, glm::vec3{ 255,0,0 }, data);
+  t0[0] = persp(y[0], center, f);
+  t0[1] = persp(y[1], center, f);
+  Bresenham(fbuf2d, t0[0].x, t0[0].y, t0[1].x, t0[1].y, glm::vec3{ 0,255,0 }, data);
+  t0[0] = persp(z[0], center, f);
+  t0[1] = persp(z[1], center, f);
+  Bresenham(fbuf2d, t0[0].x, t0[0].y, t0[1].x, t0[1].y, glm::vec3{ 0,0,255 }, data);
 
   return TRUE;
 }
