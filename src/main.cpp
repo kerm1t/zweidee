a76f631d57aa7eda2aa6f6e92b9ee2ed0094220b
wolfgang schulz <wolfgang.schulz@continental-corporation.com>
2025-06-29 07:28:50 +0200

    added glenz (zweidee concept needs cleanup)

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,24 @@
 #define FBUF2D_WIDTH  64
 #define FBUF2D_HEIGHT 48
 
-#include "zweidee.h"
+#include "zweidee.h"    // draw to 2D buffer
+#include "engine.h"     //   run a 2D game
 
 /////////////////////
 // put your code here
 /////////////////////
+
+zweidee::CEngine m_engine;
+void do_stuff_here() // b) function
+{
+  // no rapid fire :-)
+  if (GetAsyncKeyState(VK_UP))    m_engine.up();
+  if (GetAsyncKeyState(VK_DOWN))  m_engine.down();
+  if (GetAsyncKeyState(VK_LEFT))  m_engine.left();
+  if (GetAsyncKeyState(VK_RIGHT)) m_engine.right();
+
+  m_engine.move(); // move (i.e. increase playengine step) and update fbuf2d
+}
 
 void randomnoise()
 {
@@ -36,12 +49,16 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     std::cout << "init failed" << std::endl;
     return false;
   }
+//  zweidee::app_init(FBUF2D_WIDTH * 10, FBUF2D_HEIGHT * 10, hInstance, hPrevInstance, lpCmdLine, nCmdShow, WndProc);
+
+  m_engine.init(&zweidee::fbuf2d, zweidee::data);
 
   ////////////////
   // run in thread 
   ////////////////
 
-  zweidee::repeat = randomnoise;
+//  zweidee::repeat = randomnoise;
+  zweidee::repeat = do_stuff_here;
 
 
   return zweidee::app_run(hInstance);
